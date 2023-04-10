01e5b10d63e5eb01175d3ec8e17c7d6a43d58884
SlackerEva <lesiv_anna@mail.ru>
2023-04-11 00:28:17 +0300

    rewritte reducer with ts

--- src/services/reducers/reducers.ts
+++ src/services/reducers/reducers.ts
@@ -3,7 +3,7 @@ import  { fetchIngredients, fetchOrderNumber } from '../actions/actions';
 
 const initialState = {
   ingredients: [],
-  ingrData: [],
+  ingrData: <any>[],
   orderNumber: null,
   isModalOpen: false,
 }
@@ -14,18 +14,18 @@ export const ingredientsSlice = createSlice({
   reducers: {
     addIngredientData: (state, action) => {
       state.ingrData =  (action.payload.item.type === 'bun' && state.ingrData.length === 0) ? [...state.ingrData, action.payload] : state.ingrData;
-      state.ingrData =  (action.payload.item.type === 'bun' && state.ingrData.length !== 0) ? [...state.ingrData.filter(obj => obj.item.type !=='bun'), action.payload] : state.ingrData;
+      state.ingrData =  (action.payload.item.type === 'bun' && state.ingrData.length !== 0) ? [...state.ingrData.filter((obj: any) => obj.item.type !=='bun'), action.payload] : state.ingrData;
       state.ingrData =  (action.payload.item.type !== 'bun' && state.ingrData.length !== 0) ? [...state.ingrData, action.payload] : state.ingrData;
     },
     removeIngredientData: (state, action) => {
-      state.ingrData = state.ingrData.filter((item) => item.dragId !== action.payload);
+      state.ingrData = state.ingrData.filter((item: any) => item.dragId !== action.payload);
     },
     removeAllIngredientData: (state) => {
       state.ingrData = [];
     },
     updateIngredientData: (state, action) => {
-      const bun = action.payload.find(obj => obj.item.type === 'bun');
-      const newArr = action.payload.filter(obj => obj.item.type !== 'bun');
+      const bun = action.payload.find((obj: any) => obj.item.type === 'bun');
+      const newArr = action.payload.filter((obj: any) => obj.item.type !== 'bun');
       state.ingrData = [...newArr, bun, bun];
     },
     handleModalClose: (state) => {
@@ -47,5 +47,5 @@ export const ingredientsSlice = createSlice({
   },
 })
 
-export const { setIngredientDetail, handleModalClose, handleModalOpen, addIngredientData, removeIngredientData, updateIngredientData, getONumber, removeAllIngredientData } = ingredientsSlice.actions;
+export const { handleModalClose, handleModalOpen, addIngredientData, removeIngredientData, updateIngredientData, removeAllIngredientData } = ingredientsSlice.actions;
 export default ingredientsSlice.reducer;
